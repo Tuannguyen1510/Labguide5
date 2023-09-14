baf74638a8fd78e0e4d06ce79159b7ba7831b5bc
Tuannguyen1510 <87536692+Tuannguyen1510@users.noreply.github.com>
2023-09-14 16:09:34 +0700

    update

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,98 @@
+﻿using Labguide5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace Labguide5.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: AccountController
+        public ActionResult Index()
+        {
+            var accounts = DataLocal.GetAccounts();
+            return View(accounts);
+        }
+
+        // GET: AccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            Account a = new Account();
+            return View(a);
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public IActionResult VerifyPhone(string phone)
+        {
+           Regex _isPhone =  new Regex(@"^\?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
+            if (!_isPhone.IsMatch(phone))
+            {
+                return Json($"Số điện thoại {phone} Không đúng định dạng, VD: 0986421127 hoặc 998.421.1127");
+            }
+            return Json(true);
+        }
+    }
+}
---
+++ Models/DataLocal.cs
@@ -0,0 +1,59 @@
+﻿namespace Labguide5.Models
+{
+    public class DataLocal
+    {
+        public static List<Account> accounts = new List<Account>
+        {
+            new Account()
+            {
+                    Id = 1,
+                    FullName = "Ronaldo",
+                    Email="ronaldo@gmail.com",
+                    Phone = "0987656432",
+                    Address = "Bồ Đào Nha",
+                    Avatar = "images/avatar/bg-1.png",
+                    Birthday = new DateTime(1986/4/7),
+                    Gender ="Nam",
+                    Password="Ronaldo",
+                    Facebook="https://facebook.com",
+            },
+              new Account()
+            {
+                    Id = 2,
+                    FullName = "Ronaldo",
+                    Email="ronaldo@gmail.com",
+                    Phone = "0987656432",
+                    Address = "Bồ Đào Nha",
+                    Avatar = "images/avatar/bg-1.png",
+                    Birthday = new DateTime(1986/4/7),
+                    Gender ="Nam",
+                    Password="Ronaldo",
+                    Facebook="https://facebook.com",
+            },
+              new Account()
+            {
+                    Id = 3,
+                    FullName = "Ronaldo",
+                    Email="ronaldo@gmail.com",
+                    Phone = "0987656432",
+                    Address = "Bồ Đào Nha",
+                    Avatar = "images/avatar/bg-1.png",
+                    Birthday = new DateTime(1986/4/7),
+                    Gender ="Nam",
+                    Password="Ronaldo",
+                    Facebook="https://facebook.com",
+            },
+        };
+        public static List<Account> GetAccounts()
+        {
+            return accounts;
+        }
+        public static Account? GetAccountById(int id)
+        {
+            var account = GetAccounts().FirstOrDefault(p => p.Id == id);
+            return account;
+        }
+
+
+    }
+}

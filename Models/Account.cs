59b7be1fa0260ec29dbd38b9f3df36382a16bcad
Tuannguyen1510 <87536692+Tuannguyen1510@users.noreply.github.com>
2023-09-14 11:40:38 +0700

    Add project files.

---
+++ Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labguide5.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Avatar { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+        public string Facebook { get; set; }
+
+
+    }
+}

a7e6528def3232f66228d6c05a782cd4e3843f73
Tuannguyen1510 <87536692+Tuannguyen1510@users.noreply.github.com>
2023-09-14 11:46:33 +0700

    up

--- Models/Account.cs
+++ Models/Account.cs
@@ -5,8 +5,19 @@ namespace Labguide5.Models
     public class Account
     {
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [
+           Display(Name= "Họ và tên"),
+           Required(ErrorMessage ="Họ không đc bỏ trống"),
+           MinLength(6,ErrorMessage ="Họ tên ít nhất là 6 ký tự"),
+           MaxLength(20, ErrorMessage = "Họ tên tối đa là 20 ký tự"),
+        ]
         public string FullName { get; set; }
+        [
+     Display(Name = "Địa chỉ Email"),
+     Required(ErrorMessage = "Địa chỉ Email Không đc để trống"),
+     EmailAddress(ErrorMessage ="Đại chỉ email không đúng định dạng")
+        ]
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
